2550bfa60fc1b9900ad47092f6506be689b7ef1c
Ramon Moratori <ramon.moratori@estudante.ufjf.br>
2022-10-25 15:33:40 -0300

    versao 2.0.2

--- src/main/java/br/com/paranomasia/paranomasiaforum/model/Topico.java
+++ src/main/java/br/com/paranomasia/paranomasiaforum/model/Topico.java
@@ -1,5 +1,7 @@
 package br.com.paranomasia.paranomasiaforum.model;
 
+import java.text.DecimalFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -58,7 +60,10 @@ public class Topico {
 		this.mensagem = mensagem;
 	}
 	public double getAvaliacao() {
-		return avaliacao;
+		double avaliacaoFormatada = this.avaliacao;
+		DecimalFormat formato = new DecimalFormat("#.##"); 
+		avaliacaoFormatada = Double.valueOf(formato.format(avaliacaoFormatada));
+		return avaliacaoFormatada;
 	}
 	public void setAvaliacao(double avaliacao) {
 		
